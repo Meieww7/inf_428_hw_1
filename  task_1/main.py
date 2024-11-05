9248f56e2a2a327c53984e05f83f9c91e99debd4
Meieww7 <assem.n0520@gmail.com>
2024-11-05 22:01:13 +0500

    Task #1: I decided to do the first task after a couple of failed test cases p.s:comments in russian, I hope this is allowed

---  task_1/main.py
+++  task_1/main.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def findLengthOfLCIS(self, nums):
 
-        max_length = 1
-        start = 0
+        max_length = 1 #тут хранится максимальная длина
+        start = 0 # эта переменная считает начало последовательности
 
         for i in range(1, len(nums)):
-            if nums[i] <= nums[i - 1]:
-                max_length = max(max_length, i - start)
-                start = i
+            if nums[i] <= nums[i - 1]: # конец последовательности
+                max_length = max(max_length, i - start) # вычисляем длину этой последовательности и обновляем переменную где хранится максимальная длина
+                start = i #перемещаем на текущий индекс чтобы начать отслеживать новую последовательность
 
-                return max(max_length, len(nums) - start)
+        return max(max_length, len(nums) - start) # проверяем последнюю последовательность так как она может быть самой длинной
 
 solution = Solution()
 
